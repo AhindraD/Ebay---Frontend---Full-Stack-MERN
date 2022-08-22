da026eec907055a4d5cac1157c6e78792089a645
Ahindra D <official.ahindra@gmail.com>
2022-08-23 00:05:36 +0530

    fav-path PENDING

--- src/components/MyFavs.jsx
+++ src/components/MyFavs.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function MyFavs() {
     return (
-        <div>MyFavs</div>
+        <div className='display-cont'>
+            <div className="nav-bar" >
+                <a href="/ads" className="home" >Home</a>
+                <a href="/myads" className="my-Ad" >My Ads</a>
+                <a href="/myfavs" className="my-interest" >Saved Ads</a>
+                <a href="/mysold" className="sold" >Sold</a>
+            </div>
+            <div className='ad-cont'>
+                Favs
+            </div>
+        </div>
     )
 }
